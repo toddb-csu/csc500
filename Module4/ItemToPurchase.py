c41422b305fa8e41bd641082300d82d259c7e46e
Todd Bartoszkiewicz <todd.bart@gmail.com>
2024-09-07 21:44:46 -0400

    Module 4 Added default of none and changed file name to ItemToPurchase

--- Module4/ItemToPurchase.py
+++ Module4/ItemToPurchase.py
@@ -14,12 +14,8 @@
 # Example of print_item_cost() output:
 # Bottled Water 10 @ $1 = $10
 class ItemToPurchase:
-    item_name = ""
-    item_price = 0.0
-    item_quantity = 0
-
     # constructor function
-    def __init__(self, item_name = "", item_price = 0.0, item_quantity = 0):
+    def __init__(self, item_name = "none", item_price = 0.0, item_quantity = 0):
         self.item_name = item_name
         self.item_price = item_price
         self.item_quantity = item_quantity
